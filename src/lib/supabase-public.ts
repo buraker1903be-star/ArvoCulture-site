a8ab6331062c71427f7d938e18176e558fe5f07c
Burak ERDOĞAN <buraker1903.be@gmail.com>
2026-08-27 17:18:43 +0300

    Add safe public storefront configuration

---
+++ src/lib/supabase-public.ts
@@ -0,0 +1,11 @@
+export const SUPABASE_URL =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ??
+  "https://oahshpkgdzrraqdzjqau.supabase.co";
+
+export const SUPABASE_PUBLISHABLE_KEY =
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ??
+  "sb_publishable_YEnHPOE2NQOWKwN2MI590w_FYmPWOpE";
+
+export const ARVOCULTURE_ORGANIZATION_ID =
+  process.env.ARVO_ORGANIZATION_ID ??
+  "f00ab7ef-e9be-467b-9e95-db8f753275c3";
