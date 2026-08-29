958e73579f66edadea9ab95823d0a958ac461c80
Burak ERDOĞAN <buraker1903.be@gmail.com>
2026-08-29 18:11:56 +0300

    Allow product image optimization

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
-  poweredByHeader:false,
-  images:{remotePatterns:[{protocol:"https",hostname:"oahshpkgdzrraqdzjqau.supabase.co",pathname:"/storage/v1/object/public/organization-assets/**"}]}
+  poweredByHeader: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "oahshpkgdzrraqdzjqau.supabase.co",
+        pathname: "/storage/v1/object/public/organization-assets/**",
+      },
+      {
+        protocol: "https",
+        hostname: "oahshpkgdzrraqdzjqau.supabase.co",
+        pathname: "/storage/v1/object/public/arc-product-images/**",
+      },
+    ],
+  },
 };
 export default nextConfig;
