b3fe98124bfa6b7552fbea8747775fa084086afa
Burak ERDOĞAN <buraker1903.be@gmail.com>
2026-09-05 16:51:50 +0300

    Olu dosyalari temizle

--- src/lib/supabase-public.ts
+++
@@ -1,5 +0,0 @@
-export const SUPABASE_URL = "https://oahshpkgdzrraqdzjqau.supabase.co";
-export const SUPABASE_PUBLISHABLE_KEY =
-  "sb_publishable_YEnHPOE2NQOWKwN2MI590w_FYmPWOpE";
-export const ARVOCULTURE_ORGANIZATION_ID =
-  "f00ab7ef-e9be-467b-9e95-db8f753275c3";
